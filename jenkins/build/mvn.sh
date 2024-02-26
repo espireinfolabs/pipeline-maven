27bbfa92381e619bf082d8921cd926478958bfb6
Atul Sharma <atulvsharma@gmail.com>
2024-02-26 14:38:46 -0500

    Updated mvn and test scripts

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -2,5 +2,5 @@ echo "***************************"
 echo "****** Building jar *******"
 echo "***************************"
 
-WORKSPACE=/root/jenkins/jenkins_home/workspace/maven-pipeline
+WORKSPACE=/root/jenkins/jenkins_home/workspace/pipeline-docker-maven
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
