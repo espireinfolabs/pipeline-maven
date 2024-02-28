156e557f94074e3462a118c6bb45068767f22602
Atul Sharma <atulvsharma@gmail.com>
2024-02-27 20:15:58 -0500

    updating deploy

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@ echo maven-app-image-repo > /tmp/.auth
 echo jenkins-pipeline-docker-maven-4 >> /tmp/.auth
 echo Devansh@06 >> /tmp/.auth
 
-scp -i /opt/prodkeys /tmp/.auth produser@172.174.181.188:/tmp/.auth
-scp -i /opt/prodkeys /home/jenkinsadmin/jenkins-data/pipeline/jenkins/deploy/publish produser@172.174.181.188:/tmp/publish
-ssh -i /opt/prodkeys produser@172.174.181.188 "/tmp/publish"
+scp -i /opt/prodkeys /tmp/.auth produser@172.210.73.24:/tmp/.auth
+scp -i /opt/prodkeys /home/jenkinsadmin/jenkins-data/pipeline/jenkins/deploy/publish produser@172.210.73.24:/tmp/publish
+ssh -i /opt/prodkeys produser@172.210.73.24 "/tmp/publish"
